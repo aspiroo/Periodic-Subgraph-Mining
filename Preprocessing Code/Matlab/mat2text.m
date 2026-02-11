13e269914a31ce7bcf66cb6c39101669a190dd4b
safwan <safwanismaunamin@gmail.com>
2026-02-11 19:40:27 +0600

    changing folder structure, putting all previous code files in legacy

--- Preprocessing Code/Matlab/mat2text.m
+++
@@ -1,8 +0,0 @@
-numfiles = 66;
-
-for k = 1:numfiles
-  myfilename = sprintf('drosophila_subset_t%d.mat', k);
-  load(myfilename);
-  [row col v] = find(G);
-  dlmwrite(sprintf('drosophila_subset_t%d.txt', k),[row col], 'delimiter', '\t','newline','pc');
-end
--- Preprocessing Code/Python/duplicatesRemover.py
+++
@@ -1,8 +0,0 @@
-lines = open('inputs.txt', 'r').readlines()
-
-lines_set = set(lines)
-
-out = open('output.txt', 'w')
-
-for line in lines_set:
-    out.write(line)
